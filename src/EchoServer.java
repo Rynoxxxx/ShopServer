256a00ace347b39906f7fb5212e800f89f9e471c
samuel <mail@mail.com>
2024-06-26 13:29:40 +0200

    fml

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -1,7 +1,11 @@
 public class EchoServer extends Server {
+    List warenkorb;
+    Double preis = 0.0;
 
     public EchoServer(int pPort) {
+
         super(pPort);
+        warenkorb = new List<String>();
     }
 
     @Override
@@ -12,16 +16,43 @@ public class EchoServer extends Server {
     @Override
     public void processMessage(String pClientIP, int pClientPort, String pMessage) {
         String[] nachrichtTeil = pMessage.split(":");
-        if (nachrichtTeil[0].equals("TSHIRT")) {
-            this.send(pClientIP, pClientPort, "Die Größe ist " + nachrichtTeil[1] + ", die Farbe ist " + nachrichtTeil[2] + " und es kostet 19,99€! Bitte bestätigen sie die Bestellung.");
-        }else if(nachrichtTeil[0].equals("BESTAETIGUNG")){
-            if(nachrichtTeil[1].equals("ja")){
-                this.send(pClientIP,pClientPort,"Vielen Dank für ihre Bestellung.");
 
-            } else if (nachrichtTeil[1].equals("nein")) {
-                this.send(pClientIP,pClientPort, "Deine Bestellung wurde abgebrochen.");
-            }else (this.send(pClientIP, pClientPort, "Bitte geben sie ja oder nein ein.");
+        switch (nachrichtTeil[0]) {
+            case "TSHIRT":
+                this.send(pClientIP, pClientPort, "Die Größe ist " + nachrichtTeil[1] + ", die Farbe ist " + nachrichtTeil[2] + " und es kostet 19,99€! Bitte bestätigen sie die Bestellung.");
+                String objekt = nachrichtTeil[0];
+                String groesse = nachrichtTeil[1];
+                String farbe = nachrichtTeil[2];
+                preis = preis + 19.99;
 
+                warenkorb.append(objekt + ", " + groesse + ", " + farbe + ", " + "Preis: 19.99€");
+                break;
+
+            case "BESTAETIGUNG":
+                switch (nachrichtTeil[1]) {
+                    case "ja":
+                        this.send(pClientIP, pClientPort, "Vielen Dank für ihre Bestellung.");
+                        break;
+                    case "nein":
+                        this.send(pClientIP, pClientPort, "Deine Bestellung wurde abgebrochen.");
+                        break;
+                    default:
+                        this.send(pClientIP, pClientPort, "Bitte geben sie ja oder nein ein.");
+                        break;
+                }
+
+            case "warenkorb":
+                this.send(pClientIP, pClientPort, "Hier ist der Warenkorb:");
+                for (warenkorb.toFirst(); warenkorb.hasAccess(); warenkorb.next()) {
+                    this.send(pClientIP, pClientPort, warenkorb.getContent().toString());
+
+                }
+                this.send(pClientIP, pClientPort,"Gesamtpreis: "+preis+"€");
+                break;
+
+            default:
+                this.send(pClientIP, pClientPort, "Korrigiere deine Nachricht!");
+                break;
         }
     }
 
